6be4d1d2f540c95386a38adc12f634c88cb2f25f
tinkydan <88251816+tinkydan@users.noreply.github.com>
2024-08-13 21:02:05 -0400

    Update IntegerEncoding.ino
    corrected an error where the final byte is set to not continue

--- Test_Functions/IntegerEncoding/IntegerEncoding.ino
+++ Test_Functions/IntegerEncoding/IntegerEncoding.ino
@@ -20,26 +20,49 @@
 
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/ReadASCIIString
 */
-int VQL_len=0;
-  byte Encoded[5];
+int VQL_len = 0;
+byte Encoded[7];
 // pins for the LEDs:
 
 //Klipper protocol decoder
-int64_t max_int[5] = { 95, 12287, 1572863, 201326591, 4294967295 };    // 25769803775, 3298534883327, 422212465065983};
-int64_t min_int[7] = { -32, -4096, -524288, -67108864, -2147483648 };  // -17179869184, -2199023255552 };
+int64_t max_int[8] = { 95, 12287, 1572863, 201326591, 4294967295, 25769803775, 3298534883327, 422212465065983 };
+int64_t min_int[7] = { -32, -4096, -524288, -67108864, -2147483648, -17179869184, -2199023255552 };
+
+
+
+
+#undef bitRead
+#undef bitWrite
+
+template<typename T> void bitWrite(T& value, byte bit, byte val);  // prototype
+template<typename T> void bitWrite(T& value, byte bit, byte val) {
+  if (val)
+    value |= (T(1) << bit);
+  else
+    value &= ~(T(1) << bit);
+}
+
+template<typename T> bool bitRead(T value, byte bit);  // prototype
+template<typename T> bool bitRead(T value, byte bit) {
+  return (value & ((T)1 << bit)) != 0;
+}
+
+
+
+
 int64_t VLQ(byte inBytes[5], int length) {
   bool bitvec[64];
 
   for (int i = 0; i < length; i++) {
     for (int j = 0; j < 7; j++) {
       bitvec[i * 7 + j] = bitRead(inBytes[length - 1 - i], j);
-      Serial.print(String(bitvec[i * 7 + j]) + " " );
+      Serial.print(String(bitvec[i * 7 + j]) + " ");
     }
   }
   int64_t stand_I = 0;
   for (int k = 0; k < (length * 7); k++) {
-   // bitSet(stand_I, k) = bitvec[k];
-    bitWrite(stand_I, k,  bitvec[k]);
+    // bitSet(stand_I, k) = bitvec[k];
+    bitWrite(stand_I, k, bitvec[k]);
   }
 
   if (max_int[length - 1] < stand_I) {
@@ -54,48 +77,49 @@ int64_t VLQ(byte inBytes[5], int length) {
 }
 
 void EncodeVLQ(int64_t Value) {
-  Serial.println("Encoding " +String(Value));
- bool first=1;
+  Serial.println("Encoding " + String(Value));
+  bool first = 1;
   int num_byte;
   byte holdb;
   uint64_t Unsinged_Val;
 
-  for (int i = 0; i < 5; i++) {
-    Serial.println("Looping I " +String(i));
-    if ((Value <= max_int[i]) && (Value >= min_int[i])&&first) {
+  for (int i = 0; i < 7; i++) {
+    Serial.println("Looping I " + String(i));
+    if ((Value <= max_int[i]) && (Value >= min_int[i]) && first) {
       //byte Encoded[i + 1];
-      first=0;
+      first = 0;
       num_byte = i + 1;
-      VQL_len=num_byte;
-    Serial.println("Encoding " +String(num_byte));
+      VQL_len = num_byte;
+      Serial.println("Encoding " + String(num_byte));
       if (Value < 0) {
         Value = Value - min_int[num_byte - 1] + 1 + max_int[num_byte - 1];
       }
- Serial.println("Unsigned Value " +String(Value));
- Unsinged_Val=Value;
-      for (int t = num_byte -1 ; t >= 0; t--) {
-        Serial.println("Looping t " +String(t));
-        holdb=Encoded[t];
+      Serial.println("Unsigned Value " + String(Value));
+      Unsinged_Val = Value;
+      for (int t = num_byte - 1; t >= 0; t--) {
+        Serial.println("Looping t " + String(t));
+        holdb = Encoded[t];
         //bitSet(holdb, 7) = 1;
-        bitWrite(holdb, 7,1);
+        bitWrite(holdb, 7, 1);
         for (int u = 0; u < 7; u++) {
-          bool bitval = bitRead(Unsinged_Val, (num_byte-1-t) * 7 + u);
-          Serial.print(String(u) + "_" +String(bitval) +" ");
-        //  bitSet(holdb, u) = bitval;
+          bool bitval = bitRead(Unsinged_Val, (num_byte - 1 - t) * 7 + u);
+          Serial.print(String(u) + "_" + String(bitval) + " ");
+          //  bitSet(holdb, u) = bitval;
           bitWrite(holdb, u, bitval);
-          Serial.print(holdb,HEX);
+          Serial.print(holdb, HEX);
           Serial.print(" < ");
         }
-        Encoded[t]=holdb;
+        Encoded[t] = holdb;
       }
     }
   }
-  holdb=Encoded[num_byte];
- // bitSet( holdb, 7) = 0;
+  holdb = Encoded[num_byte-1];
+  // bitSet( holdb, 7) = 0;
   bitWrite(holdb, 7, 0);
-  Encoded[num_byte]=holdb;
+  Encoded[num_byte-1] = holdb;
+  
   //holdb
- // return Encoded;
+  // return Encoded;
 }
 
 
@@ -103,7 +127,7 @@ void setup() {
   // initialize serial:
   Serial.begin(250000);
   // make the pins outputs:
-Serial.println("working");
+  Serial.println("working");
 }
 
 void loop() {
@@ -112,22 +136,24 @@ void loop() {
   while (Serial.available() > 0) {
 
     // look for the next valid integer in the incoming serial stream:
-    int32_t Value = Serial.parseInt();
+    int64_t Value = Serial.parseInt();
+    //  int64_t Value = -2199023255550 ;
+    //int64_t Value = 6298534883327;
     // do it again:
 
-Serial.println("Received: " + String(Value));
+    Serial.println("Received: " + String(Value));
     // look for the newline. That's the end of your sentence:
     if (Serial.read() == '\n') {
       // constrain the values to 0 - 255 and invert
       // if you're using a common-cathode LED, just use "constrain(color, 0, 255);"
-Serial.println("Encoding: ");
+      Serial.println("Encoding: ");
 
       // print the three numbers in one string as hexadecimal:
-      EncodeVLQ(Value) ;
-      for (int j = 0; j < VQL_len ; j++){
-      Serial.println(Encoded[j],HEX);
+      EncodeVLQ(Value);
+      for (int j = 0; j < VQL_len; j++) {
+        Serial.println(Encoded[j], HEX);
       }
-      Serial.println("Transformedbacl  " + String(VLQ(Encoded,VQL_len )) );
+      Serial.println("Transformedbacl  " + String(VLQ(Encoded, VQL_len)));
     }
   }
 }
