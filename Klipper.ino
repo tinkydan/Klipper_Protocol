023042849e1282ba957e772ea48d54a19e9b2216
tinkydan <88251816+tinkydan@users.noreply.github.com>
2024-08-13 21:04:10 -0400

    Update Klipper.ino
    rolled in changes from testing to the mane function

--- Klipper.ino
+++ Klipper.ino
@@ -1,17 +1,47 @@
+*/
+int VQL_len = 0;
+byte Encoded[7];
+// pins for the LEDs:
+
 //Klipper protocol decoder
-signed long max_int[5] = { 95, 12287, 1572863, 201326591, 4294967295 };    // 25769803775, 3298534883327, 422212465065983};
-signed long min_int[7] = { -32, -4096, -524288, -67108864, -2147483648 };  // -17179869184, -2199023255552 };
-int64_t VLQ(byte inBytes[], int length) {
-  bool bitvec[];
+int64_t max_int[8] = { 95, 12287, 1572863, 201326591, 4294967295, 25769803775, 3298534883327, 422212465065983 };
+int64_t min_int[7] = { -32, -4096, -524288, -67108864, -2147483648, -17179869184, -2199023255552 };
+
+
+
+
+#undef bitRead
+#undef bitWrite
+
+template<typename T> void bitWrite(T& value, byte bit, byte val);  // prototype
+template<typename T> void bitWrite(T& value, byte bit, byte val) {
+  if (val)
+    value |= (T(1) << bit);
+  else
+    value &= ~(T(1) << bit);
+}
+
+template<typename T> bool bitRead(T value, byte bit);  // prototype
+template<typename T> bool bitRead(T value, byte bit) {
+  return (value & ((T)1 << bit)) != 0;
+}
+
+
+
+
+int64_t VLQ(byte inBytes[5], int length) {
+  bool bitvec[64];
 
   for (int i = 0; i < length; i++) {
     for (int j = 0; j < 7; j++) {
       bitvec[i * 7 + j] = bitRead(inBytes[length - 1 - i], j);
+      Serial.print(String(bitvec[i * 7 + j]) + " ");
     }
   }
   int64_t stand_I = 0;
   for (int k = 0; k < (length * 7); k++) {
-    bitSet(stand_I, k) = bitvec[k];
+    // bitSet(stand_I, k) = bitvec[k];
+    bitWrite(stand_I, k, bitvec[k]);
   }
 
   if (max_int[length - 1] < stand_I) {
@@ -25,25 +55,48 @@ int64_t VLQ(byte inBytes[], int length) {
   return stand_I;
 }
 
-byte EncodeVLQ(int64_t Value) {
+void EncodeVLQ(int64_t Value) {
+  Serial.println("Encoding " + String(Value));
+  bool first = 1;
+  int num_byte;
+  byte holdb;
+  uint64_t Unsinged_Val;
 
-
-  for (int i = 0; i < 5; i++) {
-    if ((Value <= max_int[i]) && (Value >= min_int[i])) {
-      byte Encoded[i + 1] int num_byte = i + 1;
-
+  for (int i = 0; i < 7; i++) {
+    Serial.println("Looping I " + String(i));
+    if ((Value <= max_int[i]) && (Value >= min_int[i]) && first) {
+      //byte Encoded[i + 1];
+      first = 0;
+      num_byte = i + 1;
+      VQL_len = num_byte;
+      Serial.println("Encoding " + String(num_byte));
       if (Value < 0) {
-        Value = Value - min_int[num_byte - 1] + 1 + max_int[num_byte - 1]
+        Value = Value - min_int[num_byte - 1] + 1 + max_int[num_byte - 1];
       }
-
-      for (int t = num_byte - 1; t > 0; t--)
-        bitSet(Encoded[t], 7) = 1;
-      for (int u = 0; u < 7; u++) {
-        bitSet(Encoded[t], u) = bitRead(Value, t * 7 + u);
-        ;
+      Serial.println("Unsigned Value " + String(Value));
+      Unsinged_Val = Value;
+      for (int t = num_byte - 1; t >= 0; t--) {
+        Serial.println("Looping t " + String(t));
+        holdb = Encoded[t];
+        //bitSet(holdb, 7) = 1;
+        bitWrite(holdb, 7, 1);
+        for (int u = 0; u < 7; u++) {
+          bool bitval = bitRead(Unsinged_Val, (num_byte - 1 - t) * 7 + u);
+          Serial.print(String(u) + "_" + String(bitval) + " ");
+          //  bitSet(holdb, u) = bitval;
+          bitWrite(holdb, u, bitval);
+          Serial.print(holdb, HEX);
+          Serial.print(" < ");
+        }
+        Encoded[t] = holdb;
       }
     }
   }
-  bitSet(Encoded[num_byte], 7) = 0;
-  return Encoded;
+  holdb = Encoded[num_byte-1];
+  // bitSet( holdb, 7) = 0;
+  bitWrite(holdb, 7, 0);
+  Encoded[num_byte-1] = holdb;
+  
+  //holdb
+  // return Encoded;
 }
